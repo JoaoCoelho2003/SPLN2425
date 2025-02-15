84a5e0f3b2c68b22d31bd980e6c651224fe46cdb
jojocoelho <jhcoelho03@gmail.com>
2025-02-15 16:36:44 +0000

    feat: tpc2 done

---
+++ TPC2/repetitiveLines.py
@@ -0,0 +1,35 @@
+import sys
+import argparse
+
+def remove_repetitive_lines_from_stream(input_stream, output_file, keep_spaces, comment_empty):
+    seen_lines = set()
+    with open(output_file, 'w') as outfile:
+        for line in input_stream:
+            stripped_line = line.rstrip('\n')
+            if not keep_spaces:
+                stripped_line = stripped_line.strip()
+            if stripped_line not in seen_lines:
+                if comment_empty and stripped_line == '':
+                    outfile.write('#\n')
+                else:
+                    outfile.write(line)
+                seen_lines.add(stripped_line)
+
+def main():
+    parser = argparse.ArgumentParser(description="Remove repetitive lines from input.")
+    parser.add_argument('input_file', nargs='?', help="Input file (default: stdin)")
+    parser.add_argument('-s', '--spaces', action='store_true', help="Consider spaces in line comparison")
+    parser.add_argument('-p', '--comment', action='store_true', help="Comment empty lines with #")
+    args = parser.parse_args()
+
+    output_file = 'output.txt'
+    
+    if args.input_file:
+        with open(args.input_file, 'r') as infile:
+            remove_repetitive_lines_from_stream(infile, output_file, args.spaces, args.comment)
+    else:
+        print("Enter the text (Ctrl-D to end input):")
+        remove_repetitive_lines_from_stream(sys.stdin, output_file, args.spaces, args.comment)
+
+if __name__ == "__main__":
+    main()
